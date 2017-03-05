e6370fee11a329a7aeec75fd11f0a662da8627a6
Nigel Peck <np.github@sharphosting.uk>
2017-03-05 11:53:01 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Site Module
+
+A Perl module for processing web sites configured using Sites Schema.
+
---
+++ Sites/Dir.pm
@@ -0,0 +1,12 @@
+package Sites::Dir;
+
+use strict;
+use warnings;
+use uft8;
+
+use Class::Accessor "antlers";
+
+has name => ( is => "rw" );
+
+
+'and they all lived happily ever after';
