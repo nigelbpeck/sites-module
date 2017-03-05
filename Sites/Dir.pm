74caea1236cd0cacc01ebc6aeb557364bf20dcc8
Nigel Peck <np.github@sharphosting.uk>
2017-03-05 13:37:48 -0600

    Fix typo importing utf8

--- Sites/Dir.pm
+++ Sites/Dir.pm
@@ -16,11 +16,12 @@ package Sites::Dir;
 
 use strict;
 use warnings;
-use uft8;
+use utf8;
 
 use Class::Accessor "antlers";
 
 has name => ( is => "rw" );
+
 
 
 'and they all lived happily ever after';
