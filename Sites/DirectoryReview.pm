b6b0247ad256878986ae884f9ceec2796f06e276
Nigel Peck <np.github@sharphosting.uk>
2017-03-05 21:37:01 -0600

    Rename Dir to DirectoryReview

--- Sites/DirectoryReview.pm
+++ Sites/DirectoryReview.pm
@@ -12,7 +12,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-package Sites::Dir;
+package Sites::DirectoryReview;
 
 use strict;
 use warnings;
